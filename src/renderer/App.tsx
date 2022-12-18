fc2024825cb5879c323aaadf7ab361f7d6d00598
Joseph Pacitto <josephpacitto@gmail.com>
2022-12-17 19:14:45 -0500

    eslint fixes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,45 +1,9 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+import Hello from './components/Hello';
 
-export default function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
@@ -47,4 +11,6 @@ export default function App() {
       </Routes>
     </Router>
   );
-}
+};
+
+export default App;
---
+++ src/renderer/components/Hello.tsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <div>memes</div>;
+};
+
+export default Hello;
